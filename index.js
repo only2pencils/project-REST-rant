375b8c7b728ee9899107db9f32476a90d18865de
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-08-17 17:35:17 -0400

    Changes

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+//  Modules and Globals
 require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// Express Settings
+app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 
+// Controllers & Routes
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
-  res.render("error404");
+  res.render("home");
 });
 
 app.get("*", (req, res) => {
-  // Let's have our 404 page route legitimately return a status code 404. We can do this by calling .status(404) on the response. You can chain this call together with .send(), and it will look like the code below.
-  // res.send('<h1>404 Page</h1>')
-  res.status(404).send("<h1>404 Page</h1>");
+  res.render("error404");
 });
 
+// Listen for Connections
 app.listen(process.env.PORT);
--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,8 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                {/* BOOTSTRAP CDN LINK (long) */}
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"></link>
             </head>
             <body>
                 {html.children}
