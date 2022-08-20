18faf5d64670aca0d6b52fa02179e1b5bf8e2c7d
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-08-20 14:12:24 -0400

    added new 'founded' field

--- controllers/places.js
+++ controllers/places.js
@@ -1,34 +1,55 @@
 const router = require("express").Router();
+const db = require("../models");
 
-// GET /places/new
+router.get("/", (req, res) => {
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
+});
+
+router.post("/", (req, res) => {
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+  // res.send("POST /places stub");
+});
+
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
-// POST
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("POST /places");
+
+router.get("/:id", (req, res) => {
+  res.send("GET /places/:id stub");
 });
 
-// GET /places
-router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/h-thai-ml-tables.png",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/coffee-cat.jpg",
-    },
-  ];
-  res.render("places/index", { places });
+router.put("/id:", (req, res) => {
+  res.send("PUT /places/:id stub");
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("DELETE /places/:id stub");
+});
+
+router.get("/:id/edit", (req, res) => {
+  res.send("GET /places/:id/rant stub");
+});
+
+router.post("/:id/rant", (req, res) => {
+  res.send("GET /places/:id/rant stub");
+});
+
+router.delete("/:id/rant/:rantId", (req, res) => {
+  res.send("GET /places/:id/rant/:rantId stub");
 });
 
 module.exports = router;
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,7 +3,13 @@ function index (data) {
         return (
             <div className="col-sm-6">
                 <h2>{place.name}</h2>
+                <p className="text-center">
+                    {place.cuisines}
+                </p>
                 <img src={place.pic} alt={place.name}/>
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
