6b1bcec7dd0224c769bc9a60105acf9da0261792
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-09-02 02:25:12 -0400

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -8,11 +8,6 @@ router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
-// NEW
-router.get("/new", (req, res) => {
-  res.render("places/new");
-});
-
 router.post("/", (req, res) => {
   // console.log(req.body);
   if (!req.body.pic) {
@@ -29,6 +24,11 @@ router.post("/", (req, res) => {
   res.redirect("/places");
 });
 
+// NEW
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
 router.get("/:id", (req, res) => {
   let id = Number(req.params.id);
   if (isNaN(id)) {
@@ -36,7 +36,54 @@ router.get("/:id", (req, res) => {
   } else if (!places[id]) {
     res.render("error404");
   } else {
-    res.render("places/show", { place: places[id] });
+    // places.splice(id, 1);
+    // res.redirect("/places");
+    res.render("places/show", { place: places[id], id });
+  }
+});
+
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id], id });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    places.splice(id, 1);
+    res.redirect("/places");
+  }
+  // res.send("DELETE /places/:id stub");
+});
+
+router.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    if (!req.body.pic) {
+      req.body.pic = "http://placekitten.com/250/250";
+    }
+    if (!req.body.city) {
+      req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+      req.body.state = "USA";
+    }
+    places[id] = req.body;
+    res.redirect("/places/${id}");
   }
 });
 
@@ -110,14 +157,6 @@ module.exports = router;
 
 // router.put("/id:", (req, res) => {
 //   res.send("PUT /places/:id stub");
-// });
-
-// router.delete("/:id", (req, res) => {
-//   res.send("DELETE /places/:id stub");
-// });
-
-// router.get("/:id/edit", (req, res) => {
-//   res.send("GET /places/:id/rant stub");
 // });
 
 // router.post("/:id/rant", (req, res) => {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Modules and Globals
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const methodOverride = require("method-override");
 
 // Express Settings
 app.set("views", __dirname + "/views");
@@ -9,6 +9,7 @@ app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // Controllers & Routes // PLACES //
 // app.use("/index", require("./controllers/places/index"));
