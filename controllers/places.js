cd6fef761ca8525082dbf568b11ada892ec35ef8
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-09-02 01:17:07 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -8,6 +8,7 @@ router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
+// NEW
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
@@ -26,6 +27,17 @@ router.post("/", (req, res) => {
   }
   places.push(req.body);
   res.redirect("/places");
+});
+
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id] });
+  }
 });
 
 module.exports = router;
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,26 +3,31 @@ const Def = require('../default')
 
 function index (data) {
     let placesFormatted = data.places.map((place, index) => {
-        return (
-            <div key='1' className="col-sm-6">
-                <h2 className="text-center">{place.name}</h2>
-                 <p className="text-center">
-                     {place.cuisines}
-                 </p>
-                 <img src={place.pic} alt={place.name} className="center" width="100%"/>
-                 <p className="text-center">
-                     Located in {place.city}, {place.state}
-                 </p>
-            </div>
-        )
-    })
+  return (
+    <div className="col-sm-6">
+      <h2>
+        <a href={`/places/${index}`} >
+          {place.name}
+        </a>
+      </h2>
+      <p className="text-center">
+        {place.cuisines}
+      </p>
+      <img src={place.pic} alt={place.name} />
+      <p className="text-center">
+        Located in {place.city}, {place.state}
+      </p>
+    </div>
+  )
+})
+
     return (
         <Def>
             <main>
                 <h1> Places to Rant or Rave About </h1>
                     <div className="row">
                         {placesFormatted}
-                    </div>               
+                </div>               
             </main>
         </Def>
     )
