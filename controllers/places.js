e72b134120ae8e08de89e24d1c681637f5af02fb
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-09-03 15:17:37 -0400

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -73,8 +73,10 @@ router.put("/:id", (req, res) => {
   } else if (!places[id]) {
     res.render("error404");
   } else {
+    // Dig into req.body and make sure data is valid
     if (!req.body.pic) {
-      req.body.pic = "http://placekitten.com/250/250";
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
     }
     if (!req.body.city) {
       req.body.city = "Anytown";
@@ -82,8 +84,10 @@ router.put("/:id", (req, res) => {
     if (!req.body.state) {
       req.body.state = "USA";
     }
+
+    // Save the new data into places[id]
     places[id] = req.body;
-    res.redirect("/places/${id}");
+    res.redirect(`/places/${id}`);
   }
 });
 
